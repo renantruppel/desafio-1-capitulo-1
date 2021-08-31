3b5072e2bb95f894ca8977071e7ef3396859b308
Renan Truppel Ayoub <r_truppel20@hotmail.com>
2021-08-31 18:25:17 -0300

    adicionado ferramentas de testes

--- src/index.js
+++ src/index.js
@@ -46,13 +46,12 @@ app.post('/users', check_Repeated_Username, (req, res) => {
 
 app.get('/todos',check_If_Username_Exists, (req, res) => {
 
-    const userReturn = {
-        username: req.user.username,
-        todos: req.user.todos
+    if(req.user.todos.count = 0)
+    {
+        return res.status(204).json()
     }
-    
     return res.status(200).json(
-        userReturn
+        req.user.todos
     )
 })
 
@@ -64,7 +63,7 @@ app.post('/todos', check_If_Username_Exists, (req, res) => {
         title: title,
         done: false,
         deadline: new Date(deadline),
-        created_At: new Date()
+        created_at: new Date().toString()
     }
     req.user.todos.push(todo)
     res.status(201).json(todo)
@@ -73,7 +72,7 @@ app.post('/todos', check_If_Username_Exists, (req, res) => {
 
 app.put('/todos/:id', check_If_Username_Exists,(req, res) => {
     const { title, deadline } = req.body
-    const id = req.user.todos.map(todo => {
+    req.user.todos.map(todo => {
         if(todo.id === req.params.id) {
             todo.title = title
             todo.deadline = new Date(deadline)
@@ -81,35 +80,17 @@ app.put('/todos/:id', check_If_Username_Exists,(req, res) => {
             return res.status(201).json(todoChanged)
         }
     })
-
-    if(!id) {
-        return res.status(404).json({error: "id não encontrado"})
-    }
+    return res.status(404).json({error: "id não encontrado"})
+    
 })
 
 app.patch('/todos/:id/done', check_If_Username_Exists, (req, res) => {
-    const id = req.user.todos.map(todo => {
+    req.user.todos.map(todo => {
         if(todo.id === req.params.id) {
             todo.done = true
             const todoChanged = todo
-            console.log(users)
-            console.log(users.todos)
-            return res.status(201).json(todoChanged)
-        }
-    })
-
-    if(!id) {
-        return res.status(404).json({error: "id não encontrado"})
-    }
-})
-
-app.delete('/todos/:id', check_If_Username_Exists,(req, res) => {
-    const id = req.user.todos.map((todo, index) => {
-        if(todo.id === req.params.id) {
-            console.log(user.todos)
-            user.todos.splice(index, 1)
-            const todoChanged = todo
-
+            //console.log(users)
+            //console.log(users.todos)
             return res.status(201).json(todoChanged)
         }
     })
@@ -117,59 +98,20 @@ app.delete('/todos/:id', check_If_Username_Exists,(req, res) => {
     return res.status(404).json({error: "id não encontrado"})
 })
 
-/*
-const cpfVerify = ((req, res, next) => {
-    const cpf = req.params.cpf
-    const customerExists = customers.find(customer => customer.cpf === cpf)
-        if(!customerExists) { 
-            return res.status(400).json({error: "não existe"})
+app.delete('/todos/:id', check_If_Username_Exists,(req, res) => {
+    const id = req.user.todos.map((todo, index) => {
+        if(todo.id === req.params.id) {
+            //console.log(user.todos)
+            req.user.todos.splice(index, 1)
+            const todoChanged = todo
+            return res.status(204).json([])
         }
-        req.cpf = cpf
-        req.customer = customerExists
-        next()
+
     })
 
-
-app.post('/account', (req, res) => {
-    const {cpf, nome} = req.body
-
-    const customerExists = customers.some(customer => customer.cpf === cpf)
-
-    if(customerExists) { 
-        return res.status(400).json({error: "ja existe"})
-    }
-
-    const id = uuidv4()
-    customers.push({
-        nome: nome,
-        cpf: cpf,
-        id: id,
-        saldo: 0,
-        dataCriacao: (new Date().toString()),
-        statement: []}
-        )
-    return res.send(customers)
+    return res.status(404).json({error: "id não encontrado"})
 })
-
-app.get('/statement/:cpf', cpfVerify, (req, res) => {
-    return res.status(200).json(req.customer.statement)
-})
-
-app.get('/account/saldo/:cpf', cpfVerify, (req, res) => {
-    return res.status(200).json(req.customer.saldo)
-})
-
-app.put('/account/deposito/:cpf', cpfVerify, (req, res) => {
-  
-    req.customer.saldo = req.customer.saldo + parseInt(req.body.deposito)
-    return res.status(200).json(req.customer)
-})
-
-app.put('/account/saque/:cpf', cpfVerify, (req, res) => {
-  
-    req.customer.saldo = req.customer.saldo - parseInt(req.body.saque)
-    return res.status(200).json(req.customer)
-})
-*/
 
 app.listen(3000)
+
+module.exports = {app}
