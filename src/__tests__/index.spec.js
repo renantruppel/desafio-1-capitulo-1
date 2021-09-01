ca5064929092425929b443af358f11d3d0493ce4
Renan Truppel Ayoub <r_truppel20@hotmail.com>
2021-09-01 12:24:02 -0300

    mudança na descrição

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -239,37 +239,3 @@ describe('Todos', () => {
   });
 
 });
-
-describe('Users', () => {
-  it("should be able to create a new user", async() => {
-      const userCreated = await request(app)
-      .post('/users')
-      .send({
-        name: 'John Doe10',
-        username: 'user10' 
-      });
-  
-      expect(validate(userCreated.body.id)).toBe(true);
-      expect(userCreated.body).toMatchObject(
-        {
-          name: userCreated.body.name,
-          username: userCreated.body.username,
-          id: userCreated.body.id,
-          todos: []
-        });
-  
-        expect(userCreated.status).toBe(201);
-    });
-  
-    it("should be able to create a new user", async() => {
-      const userRepeated = await request(app)
-      .post('/users')
-      .send({
-        name: 'John Doe10',
-        username: 'user10' 
-      })
-      .expect(400);
-  
-      expect(userRepeated.body.error).toBeTruthy();
-    });
-  })
